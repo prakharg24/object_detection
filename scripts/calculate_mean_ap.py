mean ap calculation error fixed

scripts/calculate_mean_ap.py
@@ -1,4 +1,6 @@
 import numpy as np
+import argparse
+from utils import read_json
 
 def calculate_overlap(gt_ele, pred_ele):
 	x1_t, y1_t, x2_t, y2_t = int(gt_ele['xmin']), int(gt_ele['ymin']), int(gt_ele['xmax']), int(gt_ele['ymax'])
@@ -18,37 +20,100 @@ def calculate_overlap(gt_ele, pred_ele):
 	iou = inter_area / (true_box_area + pred_box_area - inter_area)
 	return iou
 
-def get_score(gt_bbox, pred_bbox):
-	iou = []
-	for i in range(10):
-		iou.append(0.5 + i*0.05)
+def get_score(gt_bbox, pred_bbox_orig, iou_thres, conf_thr):
 
+	pred_bbox = []
+	for ele in pred_bbox_orig:
+		if(ele[5]>=conf_thr):
+			pred_bbox.append(ele)
 
-	for iou_thres in iou:
-		
-		tp, fp, fn = 0, 0
+	tp, fp, fn = 0, 0, 0
 
-		for pred_ele in pred_bbox:
-			found_match = False
-			for gt_ele in gt_bbox:
-				calc_iou = calculate_overlap(gt_ele, pred_ele)
-				if(calc_iou > iou_thres):
-					if(gt_ele['class']==pred_ele[4]):
-						tp += 1
-						found_match = True
-						break
-			if(not found_match):
-				fp += 1
-
+	for pred_ele in pred_bbox:
+		found_match = False
 		for gt_ele in gt_bbox:
-			found_match = False
-			for pred_ele in pred_bbox:
-				calc_iou = calculate_overlap(gt_ele, pred_ele)
-				if(calc_iou > iou_thres):
-					if(gt_ele['class']==pred_ele[4]):
-						found_match = True
-						break
-			if(not found_match):
-				fn += 1
+			calc_iou = calculate_overlap(gt_ele, pred_ele)
+			if(calc_iou > iou_thres):
+				if(gt_ele['class']==pred_ele[4]):
+					tp += 1
+					found_match = True
+					break
+		if(not found_match):
+			fp += 1
+
+	for gt_ele in gt_bbox:
+		found_match = False
+		for pred_ele in pred_bbox:
+			calc_iou = calculate_overlap(gt_ele, pred_ele)
+			if(calc_iou > iou_thres):
+				if(gt_ele['class']==pred_ele[4]):
+					found_match = True
+					break
+		if(not found_match):
+			fn += 1
 
 	return tp, fp, fn
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--input_json', type=str, required=True, help='Input json file address')
+	
+	args = parser.parse_args()
+
+	compl_data = read_json(args.input_json)
+	compl_data = compl_data['data']
+
+	iou = []
+	for i in range(1):
+		iou.append(0.5 + i*0.05)
+	avg_prec_arr = []
+
+	thr_dict = {}
+
+    for val in compl_data:
+        for pred_info in val['pred_anno']:
+        	score = pred_info[5]
+            if score not in thr_dict:
+                thr_dict[score] = 1
+
+	for iou_thres in iou:
+		prec = []
+		recc = []
+
+		for conf_thr in thr_dict:
+			ttp, tfp, tfn = 0, 0, 0
+			for ele in compl_data:
+				tp, fp, fn = get_score(ele['annotations'], ele['pred_anno'], iou_thres, conf_thr)
+				ttp += tp
+				tfp += fp
+				tfn += fn
+
+			if((ttp+tfp)==0):
+				pr = 0
+			else:
+				pr = ttp/(ttp + tfp)
+
+			if((ttp + tfn)==0):
+				re = 0
+			else:
+				re = ttp/(ttp + tfn)
+
+			prec.append(pr)
+			recc.append(re)
+
+		precisions = np.array(prec)
+		recalls = np.array(recc)
+		prec_at_rec = []
+		for recall_level in np.linspace(0.0, 1.0, 11):
+			try:
+				args = np.argwhere(recalls >= recall_level).flatten()
+				prec = max(precisions[args])
+			except ValueError:
+				prec = 0.0
+			prec_at_rec.append(prec)
+		avg_prec = np.mean(prec_at_rec)
+		avg_prec_arr.append(avg_prec)
+
+	print(avg_prec_arr)
+	print(np.mean(avg_prec_arr))